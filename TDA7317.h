1ae49abe8e58ec16748b46ef478397378f7a8b06
Alexander Liman <liman324@yandex.ru>
2019-05-18 13:38:29 +0600

    Create TDA7317.h

---
+++ TDA7317.h
@@ -0,0 +1,22 @@
+// liman324@yandex.ru rcl-radio.ru
+
+
+#ifndef TDA7317_H
+#define TDA7317_H
+
+#define TDA7317_address 0x86 // адрес 0b10000100
+
+
+#include <Arduino.h>
+class TDA7317
+{
+  public:
+    TDA7317();
+        void setVolume(int vol);        // 0...47   
+        void setBand(int band, int db); // 0...4 (1...5 полоса) , -7...+7 (-14...+14 дБ)
+	
+  private:
+	void writeWire(char a);
+};
+	
+#endif //TDA7317_H
