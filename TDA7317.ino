dc35944687c5af151de7224b294ed122f3386bb3
Alexander Liman <liman324@yandex.ru>
2019-05-31 09:01:22 +0600

    Update TDA7317.ino

--- TDA7317.ino
+++ TDA7317.ino
@@ -9,11 +9,12 @@ void setup(){
 
 void loop(){}
 
-void audio(){
-  tda.setVolume(47); // громкость 0...47
-  tda.setBand(0,0);  // номер полосы, дБ (-7...+7)
-  tda.setBand(1,0);
-  tda.setBand(2,0);
-  tda.setBand(3,0);
-  tda.setBand(4,0);
+void audio()
+  tda.setVolume(35); // громкость 0...47
+  /////////// номер полосы, дБ (-7...+7) //////////
+  tda.setBand(0,0);  // 10000 Hz
+  tda.setBand(1,0);  // 260   Hz
+  tda.setBand(2,0);  // 1000  Hz
+  tda.setBand(3,0);  // 3000  Hz
+  tda.setBand(4,0);  // 60    Hz
 }
